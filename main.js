cfeb7ef9018d9e453caec8fb84304ece8f541c42
BankMali <watchira_5@hotmail.com>
2023-04-26 00:21:11 +0700

    add player turn and comment

--- main.js
+++ main.js
@@ -1,125 +1,157 @@
 
 function App() {
-    const Int_Board = {a1: '' ,
-a2: '',
-a3: '',
-b1: '',
-b2: '',
-b3: '',
-c1: '',
-c2: '',
-c3: '',
-}
 
-const initialTitle = 'TiC-TaC-ToE'
+    const initialTitle = 'TiC-TaC-ToE';
+    const Int_Board = {
+                        a1: '',a2: '',a3: '',
+                        b1: '',b2: '',b3: '',
+                        c1: '',c2: '',c3: '',
+                    };
+
+    const [turn, setTurn] = React.useState(true); // for Check X or o
+    const [isWin, setIsWin] = React.useState(false); // for Check win or lose
+    const [board, setBoard] = React.useState(Int_Board); // for Show and update board game
+    const [title, setTitle] = React.useState(initialTitle); // for Show and change title
+
     
-    const [turn, setTurn] = React.useState(true);
-    const [isWin, setIsEnd] = React.useState(false)
-    const [board, setBoard] = React.useState(Int_Board);
-    const [title, setTitle] = React.useState(initialTitle);
-    const handleToggle = () => setBoard(!board);
+    const [showPlayer,setShowPlayer] = React.useState(true);
+    const [whoPlayer,setWhoPlayer] = React.useState(false);
+    // const handleToggle = () => setBoard(!board);
+
+
+
+    // Button reset board to begin value
     const BtnRest = (e) => {
-        setBoard(Int_Board)
-        setIsEnd(false)
-        setTitle(initialTitle)
-    }
-
-const win = [['a1','a2','a3'],['b1','b2','b3'],['c1','c2','c3'],['a1','b1','c1'],['a2','b2','c2'],['a3','b3','c3'],['a1','b2','c3'],['a3','b2','c1']];
-
-
-
-    const updateBoard = (e) => {
-        // console.log('xxx --->',e.target.name)
-        
-        // check availble or not
-            if(board[e.target.name] === '' && isWin !== true){
-                console.log('empty !!')
-
-                // check if turn == true --> 'X' turn
-                // check if turn == false --> 'O' turn
-                let player
-                player = turn ? 'X' : 'O';
-                
-                // update board
-                setBoard({...board, [e.target.name]: player});
-                // dummy value for current round
-                let tempBoard = {...board, [e.target.name]: player}
-
-                win.forEach(el => {
-                    // let draw = {a1: ('X' || 'O') ,
-                    // a2: ('X' || 'O'),
-                    // a3: ('X' || 'O'),
-                    // b1: ('X' || 'O'),
-                    // b2: ('X' || 'O'),
-                    // b3: ('X' || 'O'),
-                    // c1: ('X' || 'O'),
-                    // c2: ('X' || 'O'),
-                    // c3: ('X' || 'O'),
-                    // };
-                    // console.log('to win', tempBoard[el[0]])
-                    // console.log('to win', tempBoard[el[1]])
-                    // console.log('to win', tempBoard[el[2]])
-                    console.log('--------------- turn ', player)
-                    if(player === tempBoard[el[0]] && player === tempBoard[el[1]] && player === tempBoard[el[2]]){
-                        console.log(`----> player ${player} win !`)
-                        // setBoard(Int_Board)
-                        setIsEnd(true)
-                        setTitle(`Player ${player} win !`)
-            
-                    } else {///if (tempBoard === draw)/// {
-                        // setIsEnd(true)
-                        // setTitle(`Draw`)
-
-                    }
-          
-                });
-
-                // };
-
-                // toggle between turn
-                setTurn(!turn)
-                // console.log(player)
-            } 
-            // else {
-            //     console.log('already placed !!')
-            // }
-            // console.log('on click')
+        setTurn(true) // X start
+        setBoard(Int_Board) // board to ''
+        setIsWin(false) // board input avilable
+        setTitle(initialTitle) // initialTitle = 'TiC-TaC-ToE'
+        setWhoPlayer(false) 
+        setShowPlayer(true) //
     };
 
-    // if
+    // obj win case
+    const winCase = [   ['a1','a2','a3'],
+                        ['b1','b2','b3'],
+                        ['c1','c2','c3'],
+                        ['a1','b1','c1'],
+                        ['a2','b2','c2'],
+                        ['a3','b3','c3'],
+                        ['a1','b2','c3'],
+                        ['a3','b2','c1']];
 
+                        // set for use when Onclick updateboard
+                        const updateBoard = (e) => {
+                            // console.log('xxx --->',e.target.name)
+                            
+                            // check availble or not
+                            if(board[e.target.name] === '' && isWin !== true){
+                                    console.log('empty !!')
+                    
+                                    // check if turn == true --> 'X' turn
+                                    // check if turn == false --> 'O' turn
+                                    let player
+                                    player = turn ? 'X' : 'O';
 
-    return (
-        <div className='container'>
-            <h1>{title}</h1>
-        
+                                    // let showPlayer
+                                    let nowplayer
+                                    nowplayer = showPlayer ? 'Player O ': 'Player X' ;
+                                    
+                                    setShowPlayer(showPlayer)
+                                    console.log(nowplayer)
+                                    
+                                    // update board
+                                    setBoard({...board, [e.target.name]: player});
+                                    // dummy value for current round
+                                    let tempBoard = {...board, [e.target.name]: player}
+                    
+                                    winCase.forEach(el => {
+
+                                        // console.log('to win', tempBoard[el[0]])
+                                        // console.log('to win', tempBoard[el[1]])
+                                        // console.log('to win', tempBoard[el[2]])
+                                        console.log('--------------- turn ', player)
+
+                                        ///when win
+                                        if(player === tempBoard[el[0]] && player === tempBoard[el[1]] && player === tempBoard[el[2]]){
+                                            console.log(`----> player ${player} win !`)
+                                            // setBoard(Int_Board)
+                                            setIsWin(true) // board cant click more
+
+                                            setTitle(`Player ${player} win !`)
+                                            nowplayer = `Oopps!!`
+                                
+                                        } else {///test if (tempBoard === draw)/// {
+                                            //test setIsEnd(true)
+                                            //test setTitle(`Draw`)
+                    
+                                        }
+                              
+                                    });
+                    
+                                    // toggle between turn
+                                    setTurn(!turn)
+                                    setShowPlayer(!showPlayer)
+
+                                    // send update to whoPlayer
+                                    setWhoPlayer(nowplayer)
+                                    // console.log(player)
+                                } 
+                            else {
+                                    console.log('already placed !!')
+                                }
+                                console.log('on click')
+                            };
+                    
+                    
+                    
+return (
+    <div className='container'>
+        <h1>{title}</h1>
+        {/* <h1>{showPlayer}</h1> */}
+
+        <div className='player-container'>
+            <h2>{whoPlayer} </h2>
+        </div>
+
         <div className='container-xo'>
-         <button className='buttonxo' name='a1' onClick={updateBoard}>{board.a1}</button>
-         <button className='buttonxo' name='a2' onClick={updateBoard}>{board.a2}</button>
-         <button className='buttonxo' name='a3' onClick={updateBoard}>{board.a3}</button>
- 
+            <button className='buttonxo' name='a1' onClick={updateBoard}>{board.a1}</button>
+            <button className='buttonxo' name='a2' onClick={updateBoard}>{board.a2}</button>
+            <button className='buttonxo' name='a3' onClick={updateBoard}>{board.a3}</button>
         </div>
+                     
         <div className='container-xo'>
-         <button className='buttonxo' name='b1' onClick={updateBoard}>{board.b1}</button>
-         <button className='buttonxo' name='b2' onClick={updateBoard}>{board.b2}</button>
-         <button className='buttonxo' name='b3' onClick={updateBoard}>{board.b3}</button>
-  
+            <button className='buttonxo' name='b1' onClick={updateBoard}>{board.b1}</button>
+            <button className='buttonxo' name='b2' onClick={updateBoard}>{board.b2}</button>
+            <button className='buttonxo' name='b3' onClick={updateBoard}>{board.b3}</button>
         </div>
+                      
         <div className='container-xo'>
-         <button className='buttonxo' name='c1' onClick={updateBoard}>{board.c1}</button>
-         <button className='buttonxo' name='c2' onClick={updateBoard}>{board.c2}</button>
-         <button className='buttonxo' name='c3' onClick={updateBoard}>{board.c3}</button>
-     
+            <button className='buttonxo' name='c1' onClick={updateBoard}>{board.c1}</button>
+            <button className='buttonxo' name='c2' onClick={updateBoard}>{board.c2}</button>
+            <button className='buttonxo' name='c3' onClick={updateBoard}>{board.c3}</button>
         </div>
-        {/* {isWin && <button className='btnreset' onClick={BtnRest}> RESET</button>} */}
-        <button className='btnreset' onClick={BtnRest}>R e s e t</button>
+                         
+
+        <div className='btnrest-container'>
+            {/* {isWin && <button className='btnreset' onClick={BtnRest}> RESET</button>} */}
+            <button className='btnreset' onClick={BtnRest}>R e s e t</button>
         </div>
+    </div>
+
     );
 }
-
+                    
 const domRoot = document.getElementById('root');
 const root = ReactDOM.createRoot(domRoot);
 root.render(<App />);
+                    
+                    
+                    
+                    
+
+
+
 
 
 
